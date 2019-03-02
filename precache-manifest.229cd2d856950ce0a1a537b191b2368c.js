2dbe3f76c444c69d254065b0975902e7787e65d9
Stefan Dejanovski <stefan.dejanovski@symphony-solutions.eu>
2019-03-02 08:33:25 +0100

    Updates

---
+++ precache-manifest.229cd2d856950ce0a1a537b191b2368c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1776b350463a7291ad4d",
+    "url": "/MyOrganizator/static/css/main.726f66c0.chunk.css"
+  },
+  {
+    "revision": "1776b350463a7291ad4d",
+    "url": "/MyOrganizator/static/js/main.1776b350.chunk.js"
+  },
+  {
+    "revision": "4cf7e492644cfa0b5236",
+    "url": "/MyOrganizator/static/js/runtime~main.4cf7e492.js"
+  },
+  {
+    "revision": "59720898ffbb4f71b40a",
+    "url": "/MyOrganizator/static/css/2.4f32d84a.chunk.css"
+  },
+  {
+    "revision": "59720898ffbb4f71b40a",
+    "url": "/MyOrganizator/static/js/2.59720898.chunk.js"
+  },
+  {
+    "revision": "c18d7d6151527f2f08a8e3166a446ae4",
+    "url": "/MyOrganizator/index.html"
+  }
+];
